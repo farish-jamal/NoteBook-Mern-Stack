391872f7b8e501a0ade5c882c2393998845820bf
farish <developer.websites1@gmail.com>
2021-10-11 14:04:03 +0530

    upadting the note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -47,4 +47,34 @@ router.get(
   }
 );
 
+
+//ROUTE 2 : Adding a new notes : POST "/api/notes/updatenote" . login required
+router.get("/updatenote/:id", fetchuser, async (req, res) => {
+   const {title, description, tag} = req.body;
+
+   const newNote = {}
+      if(title){
+         newNote.title = title;
+      }
+      if(description){
+         newNote.description = description;
+      }
+      if(tag){
+         newNote.tag = tag;
+      }
+
+   // Find a note to be updated;
+   let note = await Note.findById(req.params.id)
+   if(!note){
+     return res.status(404).send("Not Found");
+   }
+   if(note.user.toString() !== req.user.id){
+      return res.status(401).send("Not Allowed");
+   }
+
+   note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new: true})
+
+   res.json({note})
+ });
+
 module.exports = router;
